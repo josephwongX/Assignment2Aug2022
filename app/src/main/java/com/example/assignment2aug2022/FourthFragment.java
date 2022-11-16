7cc21a05521241d9af49bb8a50c9e95351a10f1f
MZK0224 <zk_999@outlook.com>
2022-11-16 13:41:23 +0800

    nice job

--- app/src/main/java/com/example/assignment2aug2022/FourthFragment.java
+++ app/src/main/java/com/example/assignment2aug2022/FourthFragment.java
@@ -1,14 +1,16 @@
 package com.example.assignment2aug2022;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import androidx.fragment.app.Fragment;
-import java.io.*;
+
 import java.util.ArrayList;
 
 public class FourthFragment extends Fragment {
@@ -36,5 +38,27 @@ public class FourthFragment extends Fragment {
         list.add("Logout");
         arrayAdapter=new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,list);
         lv_menulist.setAdapter(arrayAdapter);
+
+        lv_menulist.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent,View view,int position,long id){
+                if(position==0){
+                    //profile
+                    startActivity(new Intent(getActivity(), Profile.class));
+                }else if(position==1){
+                    //donor card
+                    startActivity(new Intent(getActivity(), Profile.class));
+                }else if (position==2){
+                    //FAQs
+                    startActivity(new Intent(getActivity(), FAQs.class));
+                }else if (position==3){
+                    //how to use
+                    startActivity(new Intent(getActivity(), Profile.class));
+                }else if (position==4){
+                    //logout
+                    startActivity(new Intent(getActivity(), Profile.class));
+                }
+            }
+        });
     }
 }
