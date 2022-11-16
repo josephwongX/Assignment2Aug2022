4d5b85e89882287a1cb11818fdd393b6b281a0e0
MZK0224 <zk_999@outlook.com>
2022-11-16 15:06:17 +0800

    nice job

---
+++ app/src/main/java/com/example/assignment2aug2022/FAQs.java
@@ -0,0 +1,14 @@
+package com.example.assignment2aug2022;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FAQs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_faqs);
+    }
+}
