6d947fa28fd374039f0c089bdd7a36b23d3fc532
Joseph Wong Xin Xiang <88240743+josephwongX@users.noreply.github.com>
2022-11-16 23:17:22 +0800

    Fix Picasso dependencies issues and Firebase database wrong url

--- app/src/main/java/com/example/assignment2aug2022/MainActivity.java
+++ app/src/main/java/com/example/assignment2aug2022/MainActivity.java
@@ -3,12 +3,9 @@ package com.example.assignment2aug2022;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
-import android.widget.RelativeLayout;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -16,29 +13,16 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-
-import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
-
-    FirebaseDatabase firebaseDatabase;
-    DatabaseReference databaseReference;
-    private FirebaseAuth mAuth;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        firebaseDatabase = FirebaseDatabase.getInstance("https://console.firebase.google.com/project/assignment2aug2022-4c55f/database/assignment2aug2022-4c55f-default-rtdb/data/~2F");
-        mAuth = FirebaseAuth.getInstance();
 
         getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
         getSupportActionBar().setCustomView(R.layout.action_bar_layout);
